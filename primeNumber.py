fe56b90c7732de93f38ab685f399c0bae2c44dce
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-12-20 23:31:03 +0530

    prime number check

--- primeNumber.py
+++ primeNumber.py
@@ -1,8 +1,8 @@
 
 def prime_number(number):
     if number>1:
-        for i in range(2,number):
-            if number%i==0:
+        for j in range(2,number):
+            if number%j==0:
                 print(f"{number} is not prime ")
                 break
         else:
