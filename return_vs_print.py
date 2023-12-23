a4a8b8817ee633f035513673bba9336d25db84bd
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-12-24 02:27:36 +0530

    return vs print statement

---
+++ return_vs_print.py
@@ -0,0 +1,13 @@
+def fun1(a,b):
+    c=a+b
+    return c
+output1=fun1(5,8)
+# print(output)
+
+def fun2(d, e):
+    return d+e
+output2=fun2(output1 , 2)  # here we use the output1 as a argument 
+print(output1)
+print(output2)
+
+# when we use the return keyword ..we can use the value in future 
