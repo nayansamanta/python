229b778bb4c7c1ad66e4e9e2261cf6b0b5324b63
Nayan Samanta <137536753+nayansamanta@users.noreply.github.com>
2023-12-30 00:22:21 +0530

    oop

---
+++ OOP/opp1.py
@@ -0,0 +1,4 @@
+def display():
+    pass
+print(type(display))
+# Object-oriented programming (OOP) is a method of structuring a program by bundling related properties and behaviors into individual objects   
